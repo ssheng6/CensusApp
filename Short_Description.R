846bd1629d913516874096356d87c2d3b6d3c72f
Siyuan Sheng <siyuan.sheng@outlook.com>
2021-11-03 22:13:14 -0500

    Short_Description

---
+++ Short_Description.R
@@ -0,0 +1,2 @@
+#<Short_Description>
+#The data summarized literature research data, lab data  and USDA published data to explore the harvest, chilling, processing on the quality of meat and dairy product, at meantime,  econmic impact. we expect the project will using Shinny app to visualize the data with estimation using data training/modeling.
